c9bec5b77204bd6e3009cc95db8cec07bb0e6cf4
krishnaborude <krishanborude06@gmail.com>
2025-05-07 10:05:28 +0530

    DAY-32 is complete

---
+++ DAY-32/main.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def numTilings(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        if n == 3:
+            return 5
+
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 5
+
+        for i in range(4, n + 1):
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD
+
+        return dp[n]
